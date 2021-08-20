bea7d6c0fa33ecca9710bc7a2f31594ec1739c00
Soma Nervestopheles <nervestopheles@gmail.com>
2021-08-21 00:56:59 +0700

    Refactoring

--- main.rb
+++ main.rb
@@ -1,38 +1,48 @@
 #!/usr/bin/env ruby
 
-require 'dotenv'
-require_relative 'fgd'
+%w[
+  dotenv
+  discordrb
+].each { |gem| require gem }
+
+%w[
+  fgd
+].each { |gem| require_relative gem }
+
+Dotenv.load
 
 url = 'https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions'
 url_ru_param = '?locale=ru&country=RU&allowCountries=RU'
 
 ru_info = FreeGamesData.new(url + url_ru_param)
 
-# puts JSON.pretty_generate(ru_info.data)
-# puts JSON.pretty_generate(ru_info.raw_data)
-
-require 'discordrb'
-
-bot = Discordrb::Bot.new token: ENV['TOKEN'], prefix: '!'
+newsgirl = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], prefix: '!'
 
 mes = Discordrb::Webhooks::Embed.new
 mes.title = 'Title!'
 
+newsgirl.command(:sayonara, help_available: false) do |event|
+  break unless event.user.id.to_s == ENV['ONIICHAN']
+
+  newsgirl.send_message(event.channel.id, 'Sayonara.')
+  exit 0
+end
+
 # У обычных серверов discord ограничение 2000 символов на сообщение.
 # У серверов с nitro ограничение 4000 символов на сообщение.
-bot.message(with_text: '!raw') do |event|
+newsgirl.command(:raw) do |event|
   event.respond 'Raw response: ' + format('%.1800s', ru_info.raw_data.to_s)
 end
 
-bot.message(with_text: '!info') do |event|
-  event.channel.send_embed('qwerty', mes)
+newsgirl.command(:info) do |event|
+  event.channel.send_embed('', mes)
   event.respond 'Response length: ' + ru_info.data.to_s.length.to_s
 end
 
-bot.message(with_text: '!update') do |event|
+newsgirl.command(:update) do |event|
   event.respond 'loading...'
   event.respond JSON.pretty_generate(ru_info.update)
   event.respond 'Response length: ' + ru_info.data.to_s.length.to_s
 end
 
-bot.run
+newsgirl.run

1dcf8b8b85a58f666583fe431396bb93f3d491af
Soma Nervestopheles <nervestopheles@gmail.com>
2021-08-24 02:22:00 +0700

    Refactoring

---
+++ egs/freegame.rb
@@ -0,0 +1,88 @@
+%w[
+  http
+  json
+].each { |gem| require gem }
+
+require_relative 'gamepage'
+
+# ...
+class FreeGamesData
+  attr_reader :data,
+              :raw_data,
+              :response
+
+  def initialize(url)
+    @data = parse(request(url))
+  end
+
+  def update
+    @data = parse(request(@response.uri))
+  end
+
+  private
+
+  def request(url)
+    return @response = HTTP.get(url)
+  end
+
+  def parse(response)
+    return response if response.code != 200
+
+    processed_data = []
+    @raw_data = JSON.parse(response)
+    @raw_data['data']['Catalog']['searchStore']['elements'].each do |key, _obj|
+      processed_data.push(serialization(key)) unless key['promotions'].nil?
+    end
+    return processed_data
+  end
+
+  def serialization(input)
+    url = 'https://www.epicgames.com/store/ru/p/' +
+          input['productSlug'] + '?lang=ru'
+    return {
+      url: url,
+      logo: logo(input['keyImages']),
+      title: input['title'],
+      effectiveDate: input['effectiveDate'],
+      description: GamePage.new(url).description,
+      price: price(input['price']),
+      promotions: promotions(input['promotions'])
+    }
+  end
+
+  def logo(input)
+    input.each do |key, _obj|
+      return key['url'] if key['type'] == 'OfferImageWide'
+    end
+  end
+
+  def price(input)
+    return {
+      originalPrice: input['totalPrice']['originalPrice'],
+      discountPrice: input['totalPrice']['discountPrice']
+    }
+  end
+
+  def promotions(input)
+    return {
+      promotionalOffers: offers(input, 'promotionalOffers'),
+      upcomingPromotionalOffers: offers(input, 'upcomingPromotionalOffers')
+    }
+  end
+
+  def offers(input, offer)
+    output = []
+    input[offer].each do |key, _obj|
+      key['promotionalOffers'].each do |promo, _obj|
+        output.push(
+          {
+            startDate: promo['startDate'],
+            endDate: promo['endDate']
+          }
+        )
+      end
+    end
+    return output
+  end
+
+end
--- main.rb
+++ main.rb
@@ -7,8 +7,8 @@
 ].each { |gem| require gem }
 
 %w[
-  freegame
-  newspaper
+  egs/freegame
+  egs/newspaper
 ].each { |gem| require_relative gem }
 
 Dotenv.load
@@ -21,38 +21,47 @@ raw_output_file = 'raw_output.json'
 informations = FreeGamesData.new(url + url_ru_param)
 newsgirl = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], prefix: '!'
 
-newsgirl.command(:sayonara, help_available: false) do |event|
+newsgirl.command(
+  :sayonara,
+  help_available: false
+) do |event|
   break unless event.user.id.to_s == ENV['ONIICHAN']
 
-  newsgirl.send_message(event.channel.id, 'Sayonara.')
+  newsgirl.send_message(event.channel.id, 'sayonara.')
   exit 0
 end
 
 # У обычных серверов discord ограничение 2000 символов на сообщение.
 # У серверов с nitro ограничение 4000 символов на сообщение.
-newsgirl.command(:raw,
-                 description: 'Записать сырой ответ в лог.') do |event|
+newsgirl.command(
+  :raw,
+  description: 'Записать ответ сервера egs в лог.'
+) do |event|
   event.respond 'Raw response:'
-  event.respond format('%.1800s', informations.raw_data.to_s + '...')
+  event.respond format('%.1200s', informations.raw_data.to_s) + '...'
   File.open(raw_output_file, 'w') do |file|
     file.write(JSON.pretty_generate(informations.raw_data))
   end
   return nil
 end
 
-newsgirl.command(:update,
-                 description: 'Принудительно обновить информацию о бесплатных играх.') do |event|
+newsgirl.command(
+  :update,
+  description: 'Принудительно обновить информацию о бесплатных играх.'
+) do |event|
   event.respond 'loading...'
   informations.update
   informations.data.each do |key, _obj|
-    event.respond JSON.pretty_generate(key)
+    # event.respond JSON.pretty_generate(key)
     event.respond 'Response length: ' + key.to_s.length.to_s
   end
   return nil
 end
 
-newsgirl.command(:free,
-                 description: 'Список бесплатных игр на этой неделе.') do |event|
+newsgirl.command(
+  :free,
+  description: 'Список бесплатных игр на этой неделе.'
+) do |event|
   event.respond 'Бесплатные игры на этой неделе:'
   informations.data.each do |info, _obj|
     next if info['price']['discountPrice'] != 0
@@ -63,4 +72,4 @@ newsgirl.command(:free,
   return nil
 end
 
-newsgirl.run
+# newsgirl.run

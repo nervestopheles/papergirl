90b9805b6b97307d9d2f6ad4225b5d0aae2643b2
Soma Nervestopheles <nervestopheles@gmail.com>
2021-08-27 04:02:43 +0700

    Add promotions date

--- egs/newspaper.rb
+++ egs/newspaper.rb
@@ -1,4 +1,5 @@
 %w[
+  date
   discordrb
 ].each { |gem| require gem }
 
@@ -16,7 +17,24 @@ class Newspaper
       title: news['title'],
       description: news['description'],
       image: Discordrb::Webhooks::EmbedImage.new(url: news['logo']),
+      color: 0xb0b0b0, # just gray color
       footer: Discordrb::Webhooks::EmbedFooter.new(text: 'Epic Games Store')
+    )
+    var.add_field(
+      name: 'Дата начала акции:',
+      value: lambda do
+               ed = DateTime.parse(news['effectiveDate'])
+               return format('%s.%s.%s', ed.day.to_s, ed.month.to_s, ed.year.to_s)
+             end.call,
+      inline: true
+    )
+    var.add_field(
+      name: 'Дата конца акции:',
+      value: lambda do
+               poed = DateTime.parse(news['promotions']['promotionalOffers'][0]['endDate'])
+               return format('%s.%s.%s', poed.day.to_s, poed.month.to_s, poed.year.to_s)
+             end.call,
+      inline: true
     )
     return var
   end

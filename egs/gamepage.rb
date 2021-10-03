144e40f20a966a3e6ca3cfccf042be70b396ce71
Soma Nervestopheles <nervestopheles@gmail.com>
2021-10-03 14:43:30 +0700

    Update description getter

--- egs/gamepage.rb
+++ egs/gamepage.rb
@@ -7,13 +7,19 @@
 class GamePage
   attr_reader :description
 
+  private
+
   def initialize(url)
     @description = description_parse(url)
   end
 
   def description_parse(url)
     page = URI.open(url)
-    description = Nokogiri::HTML(page).xpath("//div[@class='css-pfxkyb']").text
+    html = Nokogiri::HTML(page)
+    about_section = html.css('div[data-component=AboutSectionLayout]')
+    about = about_section[0].text
+    genre = about_section[1].text.split(/(Жанры)|(Особенности)/)[2].split(/(?=[А-Я])/).join(', ')
+    description = about + "\n\nЖанры: " + genre
     File.delete(page)
     return description
   end
